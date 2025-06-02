47b89cd1f0182ce1e8c5bd9a027d1ece976d51ff
TBarker746 <124067574+TBarker746@users.noreply.github.com>
2025-06-02 09:00:32 +0100

    Minor fix to prevent infinite loop

--- parse.py
+++ parse.py
@@ -98,7 +98,7 @@ class Parser:
             while not (self.symbol.type == self.scanner.COMMA or self.symbol.type == self.scanner.KEYWORD):
                 self.symbol = self.scanner.get_symbol()
         elif stopping_symbol == "end":
-            while not self.symbol.ID == self.scanner.END_ID:
+            while not self.symbol.ID == self.scanner.END_ID or self.symbol.ID == self.scanner.EOF:
                 self.symbol = self.scanner.get_symbol()
 
     def _name(self):
