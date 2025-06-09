62616d04d2b3baa88dc292c62df5afba86559cf3
Timoite <ht467@cam.ac.uk>
2025-06-09 09:13:44 +0100

    added reference for internationalisation

---
+++ app_const.py
@@ -0,0 +1,10 @@
+import wx
+
+langDomain = "..." # Replace it after translation is done
+
+supportedLanguages = {u"en": wx.LANGUAGE_ENGLISH,
+                      u"zh_CN": wx.LANGUAGE_CHINESE_SIMPLIFIED,
+}
+
+
+
