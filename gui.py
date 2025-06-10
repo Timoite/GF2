ae8b8bd01d927488a2807c3221b5b4e22b602208
hw617 <124673508+hw617@users.noreply.github.com>
2025-06-10 16:26:24 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -465,7 +465,8 @@ class Gui(wx.Frame):
         left_sizer.Add(monitors_sizer, 5, wx.EXPAND | wx.ALL, 5)
         left_sizer.Add(switches_sizer, 5, wx.EXPAND | wx.ALL, 5)
         run_sizer.Add(run_text1, 0, wx.CENTER)
-        run_sizer.Add(run_cont_sizer, 0, wx.CENTER | wx.TOP, 10)
+        run_sizer.Add(run_cont_sizer, 0, 
+                      wx.CENTER | wx.TOP | wx.LEFT | wx.RIGHT, 10)
         run_sizer.Add(cycles_sizer, 0, wx.CENTER | wx.ALL, 5)
         run_sizer.Add(run_text2, 0, wx.CENTER | wx.TOP, 20)
         run_sizer.Add(play_pause_sizer, 0, wx.CENTER | wx.TOP, 10)
