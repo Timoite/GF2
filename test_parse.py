ecdc0bb91878d07483d3ac33969bb8566d09b4d8
TBarker746 <124067574+TBarker746@users.noreply.github.com>
2025-06-12 13:09:23 +0100

    Just seeing if this fixes the filepath issues

--- test_parse.py
+++ test_parse.py
@@ -26,8 +26,7 @@ def parser_factory():
         network = Network(names, devices)
         monitors = Monitors(names, devices, network)
         # Resolve path relative to the BASE folder, not the 'final' subfolder
-        base_dir = os.path.dirname(os.path.dirname(__file__))
-        file_path = os.path.join(base_dir, "test_files", filename)
+        file_path = "/home/test_files" + filename
         scanner = Scanner(file_path, names)
         return Parser(names, devices, network, monitors, scanner)
     return _create_parser
